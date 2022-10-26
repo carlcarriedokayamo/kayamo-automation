08a6deb4c287d4d27ce6d277f5e0bf73eac028df
Carl Carriedo <carl.carriedo@kayamo.com>
2022-10-26 17:05:59 +0800

    no message

--- Test Suites/TSC_Backoffice.ts
+++ Test Suites/TSC_Backoffice.ts
@@ -3,15 +3,13 @@
    <description></description>
    <name>TSC_Backoffice</name>
    <tag></tag>
-   <delayBetweenInstances>5</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>2</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>Carl</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
+            <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
@@ -20,8 +18,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>Carl</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
+            <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
--- Test Suites/TSC_E2E_New_Loan_Application.ts
+++ Test Suites/TSC_E2E_New_Loan_Application.ts
@@ -9,7 +9,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>Carl</profileName>
+            <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
